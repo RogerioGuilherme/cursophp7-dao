572c8d15c23e6036a924b12e89bc352d007295fc
Rogerio Guilherme <leonciojpa@gmail.com>
2022-07-12 13:07:06 -0300

    comando delete

--- index.php
+++ index.php
@@ -33,11 +33,21 @@ echo $root;*/
 
 //echo $aluno;
 
+//Alterar um usuário
+
+//$usuario = new Usuario();
+
+//$usuario->loadById(8);
+
+//$usuario->update("professor", "!@#$%¨&*");
+
+//echo $usuario;
+
 $usuario = new Usuario();
 
-$usuario->loadById(8);
+$usuario->loadById(9);
 
-$usuario->update("professor", "!@#$%¨&*");
+$usuario->delete();
 
 echo $usuario;
  ?>
